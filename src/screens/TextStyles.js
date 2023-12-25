0b40b9e61247d6fb97a6a131fba3f1f3c040906c
RainbowGenie <joseph.anello.jr1205@gmail.com>
2023-12-25 05:15:07 -0800

    Update css of theme and text style page.

--- src/screens/TextStyles.js
+++ src/screens/TextStyles.js
@@ -2,17 +2,16 @@ import React from "react";
 import TextStyle from "../components/video/TextStyle";
 
 export default function TextStyles() {
-
-    return (
-        <div className="h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden">
-            {/*Container*/}
-            <div className="flex w-[1600px] mx-auto h-full">
-                {/* */}
-                <div className="min-h-screen w-full flex items-center justify-center overflow-hidden">
-                    {/* Container */}
-                    <TextStyle />
-                </div>
-            </div>
+  return (
+    <div className="h-screen dark:bg-dark_bg_1 flex items-center justify-center overflow-hidden">
+      {/*Container*/}
+      <div className="flex w-full mx-auto h-full">
+        {/* */}
+        <div className="min-h-screen w-full flex items-center justify-center overflow-hidden">
+          {/* Container */}
+          <TextStyle />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
